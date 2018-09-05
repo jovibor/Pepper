e44df3a5052c88ad8d8ffea154dc4895b677e429
Jovibor <>
2018-09-05 19:48:47 +1000

    Errors (typos) in .rc, caused enormous .exe size fixed.

--- Pepper/include/resource.h
+++ Pepper/include/resource.h
@@ -25,9 +25,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define APS_NEXT_RESOURCE_VALUE        311
-#define APS_NEXT_COMMAND_VALUE         32775
-#define APS_NEXT_CONTROL_VALUE         1004
-#define APS_NEXT_SYMED_VALUE           310
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
--- Pepper/include/stdafx.h
+++ Pepper/include/stdafx.h
@@ -1,11 +1,10 @@
 #pragma once
 #define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
-#define HAS_EXCEPTIONS 0
-//#include <SDKDDKVer.h>
+#define _HAS_EXCEPTIONS 0
 #define WINVER 0x0600
 
 // turns off MFC's hiding of some common and often safely ignored warning messages
-#define AFX_ALL_WARNINGS
+#define _AFX_ALL_WARNINGS
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxcontrolbars.h>     // MFC support for ribbons and control bars
